6b9eead8d56036a8edfb45d377d3f4d7d15ebc47
Tuan Ho <htuan0889@gmail.com>
2018-07-14 15:09:21 +0700

    first commit

---
+++ NewBlogCore/NewBlogCore/Data/DbInitializer.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NewBlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBlogCore.Data
+{
+    public class DbInitializer
+    {
+        private ApplicationDbContext _context;
+
+        public DbInitializer(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private static Dictionary<string, Category> categories;
+        public static Dictionary<string, Category> Categories
+        {
+            get
+            {
+                if (categories == null)
+                {
+                    var genresList = new Category[]
+                    {
+                        new Category { CategoryName = "Milk Tea", Description="All milk tea drinks" },
+                        new Category { CategoryName = "Fruit", Description="All fruit drinks" }
+                    };
+
+                    categories = new Dictionary<string, Category>();
+
+                    foreach (Category genre in genresList)
+                    {
+                        categories.Add(genre.CategoryName, genre);
+                    }
+                }
+
+                return categories;
+            }
+        }
+
+        //public static void Seed(IApplicationBuilder applicationBuilder)
+        //public static void Seed(ApplicationDbContext context, IApplicationBuilder applicationBuilder)
+        public void Seed()
+        {
+            //ApplicationDbContext context =
+            //    applicationBuilder.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            if (!_context.Categories.Any())
+            {
+                _context.AddRange(
+                    new Category { CategoryName = "Milk Tea", Description = "All milk tea drinks" },
+                    new Category { CategoryName = "Fruit", Description = "All fruit drinks" }
+                );
+                _context.SaveChangesAsync();
+            }
+
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.AddRange(Categories.Select(c => c.Value));
+            }
+
+            if (!_context.Drinks.Any())
+            {
+                _context.AddRange
+                (
+                    new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }, new Drink
+                    {
+                        Name = "Sữa Tươi Trân Châu Đường Đen",
+                        Price = 9.99M,
+                        ShortDescription = "Cách làm Sữa Tươi Trân Châu Đường Đen",
+                        LongDescription = "Sữa tươi trân châu đường nâu đang là món hot trend trên thị trường, vị béo ngậy của sữa tươi thanh trùng, vị dai dai ngọt ngọt của trân châu đường nâu sẽ khiến bạn uống mãi không dừng. Cách làm thì vô cùng đơn giản, bạn có thể tự làm tại nhà để đảm bảo an toàn sức khỏe nhé!",
+                        Category = Categories["Milk Tea"],
+                        ImageUrl = "~Images/brew1.jpg",
+                        InStock = true,
+                        IsPreferredDrink = true,
+                        ImageThumbnailUrl = "~Images/brew1.jpg"
+                    }
+                );
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ NewBlogCore/NewBlogCore/Startup.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewBlogCore.Data;
+using NewBlogCore.Models;
+using NewBlogCore.Services;
+using NewBlogCore.Interface;
+using NewBlogCore.Repository;
+using Microsoft.AspNetCore.Http;
+
+namespace NewBlogCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        //private IConfigurationRoot _configurationRoot;
+        //public Startup(IHostingEnvironment hostingEnvironment)
+        //{
+        //    _configurationRoot = new ConfigurationBuilder()
+        //        .SetBasePath(hostingEnvironment.ContentRootPath)
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+        //}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Server configuration
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IDrinkRepository, DrinkRepository>();
+
+            services.AddScoped<SignInManager<ApplicationUser>, SignInManager<ApplicationUser>>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sp => ShoppingCart.GetCart(sp));
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+
+            services.AddTransient<DbInitializer>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app
+                            , IHostingEnvironment env
+                            , DbInitializer dbSeeder
+            )
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                   name: "drinkdetails",
+                   template: "Drink/Details/{drinkId?}",
+                   defaults: new { Controller = "Drink", action = "Details" });
+
+                routes.MapRoute(
+                    name: "categoryfilter",
+                    template: "Drink/{action}/{category?}",
+                    defaults: new { Controller = "Drink", action = "List" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            dbSeeder.Seed();
+        }
+    }
+}
